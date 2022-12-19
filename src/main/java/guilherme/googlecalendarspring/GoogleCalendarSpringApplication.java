5bfba6b598c9e9947794a69e1dbe4ab2ce5f06d4
guilherme.duane <guilherme.duane@axxiom.com.br>
2022-12-19 08:54:12 -0300

    files outside vcs

---
+++ src/main/java/guilherme/googlecalendarspring/GoogleCalendarSpringApplication.java
@@ -0,0 +1,13 @@
+package guilherme.googlecalendarspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guilherme/googlecalendarspring/GoogleCalendarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package guilherme.googlecalendarspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
